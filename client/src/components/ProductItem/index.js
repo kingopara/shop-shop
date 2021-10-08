68ee736feb38dea8a87cf5b264e17ff207018199
praiseopara <cpopara2010@yahoo.com>
2021-10-07 20:47:47 -0500

    implemented indexedDB, created the idb helper function,implemented storage for products, categories and shopping cart

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -3,8 +3,12 @@ import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
+
+  const [state, dispatch] = useStoreContext();
+  
   const {
     image,
     name,
@@ -12,8 +16,6 @@ function ProductItem(item) {
     price,
     quantity
   } = item;
-
-  const [state, dispatch] = useStoreContext();
 
   const { cart } = state;
 
@@ -28,11 +30,16 @@ function ProductItem(item) {
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
         dispatch({
           type: ADD_TO_CART,
           product: { ...item, purchaseQuantity: 1 }
         });
+        idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
   };
 
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -10,6 +10,8 @@ import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS } 
 
 import Cart from '../components/Cart';
 
+import { idbPromise } from '../utils/helpers';
+
 function Detail() {
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
@@ -30,8 +32,21 @@ function Detail() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   const addToCart = () => {
     // find the cart item with the matching id
@@ -44,11 +59,18 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
         dispatch({
           type: ADD_TO_CART,
           product: { ...currentProduct, purchaseQuantity: 1 }
         });
+        // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+        idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
@@ -57,6 +79,8 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+    // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   return (
